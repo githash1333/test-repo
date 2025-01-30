72cdd93a1629a19404314e9aa3d7b635b2703d89
unknown <manish.kamble7547@gmail.com>
2025-01-30 12:29:15 +0530

    last-commit

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+from tracemalloc import Statistic
+import webbrowser
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
